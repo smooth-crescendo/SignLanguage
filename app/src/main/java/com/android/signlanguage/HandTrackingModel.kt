ab83190d5a279d189ab2a73ba66a3af35ae59d0f
Vladislav Gladkiy <v.a.gladkiy1@gmail.com>
2021-03-23 12:00:19 +0300

    Extract hand tracking model and it's loading into separate class

---
+++ app/src/main/java/com/android/signlanguage/HandTrackingModel.kt
@@ -0,0 +1,189 @@
+package com.android.signlanguage
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.SurfaceTexture
+import android.util.Log
+import android.util.Size
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.mediapipe.components.CameraHelper
+import com.google.mediapipe.components.CameraXPreviewHelper
+import com.google.mediapipe.components.ExternalTextureConverter
+import com.google.mediapipe.components.FrameProcessor
+import com.google.mediapipe.formats.proto.LandmarkProto
+import com.google.mediapipe.framework.Packet
+import com.google.mediapipe.framework.PacketGetter
+import com.google.mediapipe.glutil.EglManager
+
+class HandTrackingModel {
+
+    private val TAG = "HandTrackingModel"
+
+    private val BINARY_GRAPH_NAME = "hand_tracking_mobile_gpu.binarypb"
+    private val INPUT_VIDEO_STREAM_NAME = "input_video"
+    private val OUTPUT_VIDEO_STREAM_NAME = "output_video"
+    private val OUTPUT_LANDMARKS_STREAM_NAME = "hand_landmarks"
+    private val INPUT_NUM_HANDS_SIDE_PACKET_NAME = "num_hands"
+    private val NUM_HANDS = 1
+    private val CAMERA_FACING = CameraHelper.CameraFacing.FRONT
+    private val FLIP_FRAMES_VERTICALLY = true
+
+    private lateinit var previewFrameTexture: SurfaceTexture
+    private lateinit var processor: FrameProcessor
+    private lateinit var eglManager: EglManager
+    private lateinit var converter: ExternalTextureConverter
+    private lateinit var cameraHelper: CameraXPreviewHelper
+    private lateinit var previewDisplayView: SurfaceView
+
+    private var handTrackingCallback: ((Array<Array<FloatArray>>) -> Unit)? = null
+
+    private var _isCameraLoaded = MutableLiveData(false)
+    val isCameraLoaded: LiveData<Boolean> = _isCameraLoaded
+
+    fun initializeConverter() {
+        converter = ExternalTextureConverter(
+            eglManager.context, 2
+        )
+        converter.setFlipY(FLIP_FRAMES_VERTICALLY)
+        converter.setConsumer(processor)
+    }
+
+    fun setupPreviewDisplayView(pdv: SurfaceView) {
+        previewDisplayView = pdv
+        previewDisplayView.visibility = View.GONE
+        previewDisplayView.alpha = 0f
+        previewDisplayView
+            .holder
+            .addCallback(
+                object : SurfaceHolder.Callback {
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        Log.d(TAG, "surface created")
+
+                        processor.videoSurfaceOutput.setSurface(holder.surface)
+                    }
+
+                    override fun surfaceChanged(
+                        holder: SurfaceHolder,
+                        format: Int,
+                        width: Int,
+                        height: Int
+                    ) {
+                        Log.d(TAG, "surface changed")
+                        val viewSize = Size(width, height)
+                        val displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize)
+                        val isCameraRotated = cameraHelper.isCameraRotated
+                        converter.setSurfaceTextureAndAttachToGLContext(
+                            previewFrameTexture,
+                            if (isCameraRotated) displaySize.height else displaySize.width,
+                            if (isCameraRotated) displaySize.width else displaySize.height
+                        )
+                        try {
+                            previewFrameTexture.updateTexImage()
+                        } catch (exception: Exception) {}
+                    }
+
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        Log.d(TAG, "surface destroyed")
+                        processor.videoSurfaceOutput.setSurface(null)
+                    }
+                })
+    }
+
+    fun startCamera(activity: Activity) {
+        cameraHelper = CameraXPreviewHelper()
+        cameraHelper.setOnCameraStartedListener { surfaceTexture ->
+            Log.d(TAG, "camera started")
+            previewFrameTexture = surfaceTexture!!
+            previewDisplayView.visibility = View.VISIBLE
+        }
+        cameraHelper.startCamera(
+            activity, CAMERA_FACING, null, null
+        )
+    }
+
+    fun initializeProcessor(context: Context?) {
+        eglManager = EglManager(null)
+        processor = FrameProcessor(
+            context,
+            eglManager.nativeContext,
+            BINARY_GRAPH_NAME,
+            INPUT_VIDEO_STREAM_NAME,
+            OUTPUT_VIDEO_STREAM_NAME
+        )
+        processor
+            .videoSurfaceOutput
+            .setFlipY(FLIP_FRAMES_VERTICALLY)
+
+        processor.setOnWillAddFrameListener {
+            if (_isCameraLoaded.value == false)
+                _isCameraLoaded.postValue(true)
+        }
+
+        val packetCreator = processor.packetCreator
+        val inputSidePackets: MutableMap<String, Packet> = HashMap()
+        inputSidePackets[INPUT_NUM_HANDS_SIDE_PACKET_NAME] = packetCreator.createInt32(NUM_HANDS)
+        processor.setInputSidePackets(inputSidePackets)
+
+        processor.addPacketCallback(
+            OUTPUT_LANDMARKS_STREAM_NAME
+        ) { packet: Packet ->
+            handsCallback(packet)
+        }
+    }
+
+    fun close() {
+        converter.close()
+    }
+
+    private fun handsCallback(packet: Packet) {
+        val multiHandLandmarks =
+            PacketGetter.getProtoVector(
+                packet,
+                LandmarkProto.NormalizedLandmarkList.parser()
+            )
+        val handLandmarks = multiHandLandmarks[0]
+
+        handTrackingCallback?.invoke(processHandLandmarks(handLandmarks))
+    }
+
+    private fun processHandLandmarks(handLandmarks: LandmarkProto.NormalizedLandmarkList): Array<Array<FloatArray>> {
+        val input = Array(1) { Array(21) { FloatArray(3) } }
+
+        for (i in 0..20) {
+            val lm = handLandmarks.getLandmark(i)
+            input[0][i][0] = lm.x
+            input[0][i][1] = lm.y
+            input[0][i][2] = lm.z
+        }
+
+        alignAxisLandmarks(input, 0)
+        alignAxisLandmarks(input, 1)
+        alignAxisLandmarks(input, 2)
+
+        return input
+    }
+
+    private fun alignAxisLandmarks(src: Array<Array<FloatArray>>, ax: Int) {
+        var min = 100f
+        for (i in 0..20) {
+            val v = src[0][i][ax]
+            if (v < min) {
+                min = v
+            }
+        }
+        for (i in 0..20) {
+            src[0][i][ax] -= min
+        }
+    }
+
+    /**
+     * @param handsLandmarks Array(1 - hands) { Array(21 - landmarks) { FloatArray(3 - axes) } }
+     */
+    fun addCallback(callback: (Array<Array<FloatArray>>) -> Unit) {
+        handTrackingCallback = callback
+    }
+}
--- app/src/main/java/com/android/signlanguage/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/signlanguage/ui/home/HomeFragment.kt
@@ -1,57 +1,37 @@
 package com.android.signlanguage.ui.home
 
-import android.graphics.SurfaceTexture
 import android.os.Bundle
 import android.util.Log
-import android.util.Size
 import android.view.*
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import com.android.signlanguage.HandTrackingModel
 import com.android.signlanguage.R
 import com.android.signlanguage.SignModelLoader
 import com.google.mediapipe.components.*
-import com.google.mediapipe.components.CameraHelper.CameraFacing
-import com.google.mediapipe.formats.proto.LandmarkProto
 import com.google.mediapipe.framework.AndroidAssetUtil
-import com.google.mediapipe.framework.Packet
-import com.google.mediapipe.framework.PacketGetter
-import com.google.mediapipe.glutil.EglManager
 import org.tensorflow.lite.Interpreter
 
 
 class HomeFragment : Fragment() {
 
+    private val TAG = "HomeFragment"
+
     private lateinit var homeViewModel: HomeViewModel
 
-    private val BINARY_GRAPH_NAME = "hand_tracking_mobile_gpu.binarypb"
-    private val INPUT_VIDEO_STREAM_NAME = "input_video"
-    private val OUTPUT_VIDEO_STREAM_NAME = "output_video"
-    private val OUTPUT_LANDMARKS_STREAM_NAME = "hand_landmarks"
-    private val INPUT_NUM_HANDS_SIDE_PACKET_NAME = "num_hands"
-    private val NUM_HANDS = 1
-    private val CAMERA_FACING = CameraFacing.FRONT
-    private val FLIP_FRAMES_VERTICALLY = true
-
-    private lateinit var previewFrameTexture: SurfaceTexture
-    private lateinit var previewDisplayView: SurfaceView
-    private lateinit var eglManager: EglManager
-    private lateinit var processor: FrameProcessor
-    private lateinit var converter: ExternalTextureConverter
-    private lateinit var cameraHelper: CameraXPreviewHelper
-
     private lateinit var viewGroup: ViewGroup
+    private lateinit var previewDisplayView: SurfaceView
     private lateinit var signButton: Button
     private lateinit var signText: TextView
     private lateinit var noCameraAccessView: TextView
     private lateinit var loadingCameraProgressBar: ProgressBar
     private lateinit var letterImage: ImageView
 
-    private var isCameraLoaded = MutableLiveData(false)
+    private var handTrackingModel = HandTrackingModel()
 
     private lateinit var signModel: Interpreter
 
@@ -63,7 +43,7 @@ class HomeFragment : Fragment() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("HomeFragment" + this.hashCode(), "onCreate")
+        Log.d(TAG + this.hashCode(), "onCreate")
         super.onCreate(savedInstanceState)
     }
 
@@ -72,7 +52,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("HomeFragment" + this.hashCode(), "onCreateView")
+        Log.d(TAG + this.hashCode(), "onCreateView")
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
@@ -88,28 +68,18 @@ class HomeFragment : Fragment() {
         signModel = SignModelLoader().load(requireActivity().assets, "model.tflite")
 
         previewDisplayView = SurfaceView(context)
-        setupPreviewDisplayView()
+        viewGroup.addView(previewDisplayView)
+        handTrackingModel.setupPreviewDisplayView(previewDisplayView)
 
         AndroidAssetUtil.initializeNativeAssetManager(context)
 
-        eglManager = EglManager(null)
-        processor = FrameProcessor(
-            context,
-            eglManager.nativeContext,
-            BINARY_GRAPH_NAME,
-            INPUT_VIDEO_STREAM_NAME,
-            OUTPUT_VIDEO_STREAM_NAME
-        )
-        processor
-            .videoSurfaceOutput
-            .setFlipY(FLIP_FRAMES_VERTICALLY)
+        handTrackingModel.initializeProcessor(context)
 
-        processor.setOnWillAddFrameListener {
-            if (isCameraLoaded.value == false)
-                isCameraLoaded.postValue(true)
+        handTrackingModel.addCallback {
+            handsCallback(it)
         }
 
-        isCameraLoaded.observe(viewLifecycleOwner) {
+        handTrackingModel.isCameraLoaded.observe(viewLifecycleOwner) {
             if (it == true) {
                 loadingCameraProgressBar.visibility = View.GONE
                 letterImage.visibility = View.VISIBLE
@@ -120,153 +90,50 @@ class HomeFragment : Fragment() {
         if (PermissionHelper.cameraPermissionsGranted(activity)) {
             noCameraAccessView.visibility = View.GONE
             loadingCameraProgressBar.visibility = View.VISIBLE
-
-        }
-
-        val packetCreator = processor.packetCreator
-        val inputSidePackets: MutableMap<String, Packet> = HashMap()
-        inputSidePackets[INPUT_NUM_HANDS_SIDE_PACKET_NAME] = packetCreator.createInt32(NUM_HANDS)
-        processor.setInputSidePackets(inputSidePackets)
-
-        processor.addPacketCallback(
-            OUTPUT_LANDMARKS_STREAM_NAME
-        ) { packet: Packet ->
-            handsCallback(packet)
         }
 
         return root
     }
 
-    private fun handsCallback(packet: Packet) {
-        val multiHandLandmarks =
-            PacketGetter.getProtoVector(
-                packet,
-                LandmarkProto.NormalizedLandmarkList.parser()
-            )
-        val handLandmarks = multiHandLandmarks[0]
-
-        val input = Array(1) { Array(21) { FloatArray(3) } }
-
-        for (i in 0..20) {
-            val lm = handLandmarks.getLandmark(i)
-            input[0][i][0] = lm.x
-            input[0][i][1] = lm.y
-            input[0][i][2] = lm.z
-        }
-
-        alignAxisLandmarks(input, 0)
-        alignAxisLandmarks(input, 1)
-        alignAxisLandmarks(input, 2)
-
+    private fun handsCallback(input: Array<Array<FloatArray>>) {
         val output = Array(1) { FloatArray(5) }
         signModel.run(input, output)
 
-        val signTextStringBuilder = StringBuilder()
         var predictedSignIndex = 0
         for (j in 0..4) {
-            signTextStringBuilder.append(output[0][j])
-            signTextStringBuilder.append("  ")
             if (output[0][j] > output[0][predictedSignIndex])
                 predictedSignIndex = j
         }
         signText.text = ('A' + predictedSignIndex).toString()
     }
 
-    private fun alignAxisLandmarks(src: Array<Array<FloatArray>>, ax: Int) {
-        var min = 100f
-        for (i in 0..20) {
-            val v = src[0][i][ax]
-            if (v < min) {
-                min = v
-            }
-        }
-        for (i in 0..20) {
-            src[0][i][ax] -= min
-        }
-    }
-
     override fun onResume() {
-        Log.d("HomeFragment" + this.hashCode(), "onResume")
+        Log.d(TAG + this.hashCode(), "onResume")
         super.onResume()
 
-        converter = ExternalTextureConverter(
-            eglManager.context, 2
-        )
-        converter.setFlipY(FLIP_FRAMES_VERTICALLY)
-        converter.setConsumer(processor)
+        handTrackingModel.initializeConverter()
 
         if (PermissionHelper.cameraPermissionsGranted(activity)) {
-            cameraHelper = CameraXPreviewHelper()
-            cameraHelper.setOnCameraStartedListener { surfaceTexture ->
-                Log.d("HomeFragment", "camera started")
-                previewFrameTexture = surfaceTexture!!
-                previewDisplayView.visibility = View.VISIBLE
-            }
-            cameraHelper.startCamera(
-                activity, CAMERA_FACING, null, null
-            )
+            handTrackingModel.startCamera(requireActivity())
         }
     }
 
     override fun onPause() {
-        Log.d("HomeFragment" + this.hashCode(), "onPause")
+        Log.d(TAG + this.hashCode(), "onPause")
         super.onPause()
-        converter.close()
+        handTrackingModel.close()
         previewDisplayView.visibility = View.GONE
     }
 
     override fun onDestroyView() {
-        Log.d("HomeFragment" + this.hashCode(), "onDestroyView")
+        Log.d(TAG + this.hashCode(), "onDestroyView")
         super.onDestroyView()
     }
 
     override fun onDestroy() {
-        Log.d("HomeFragment" + this.hashCode(), "onDestroy")
+        Log.d(TAG + this.hashCode(), "onDestroy")
         super.onDestroy()
         signModel.close()
-    }
-
-    private fun setupPreviewDisplayView() {
-        previewDisplayView.visibility = View.GONE
-        previewDisplayView.alpha = 0f
-        viewGroup.addView(previewDisplayView)
-        previewDisplayView
-            .holder
-            .addCallback(
-                object : SurfaceHolder.Callback {
-                    override fun surfaceCreated(holder: SurfaceHolder) {
-                        Log.d("HomeFragment", "surface created")
-
-                        processor.videoSurfaceOutput.setSurface(holder.surface)
-                    }
-
-                    override fun surfaceChanged(
-                        holder: SurfaceHolder,
-                        format: Int,
-                        width: Int,
-                        height: Int
-                    ) {
-                        Log.d("HomeFragment", "surface changed")
-                        val viewSize = Size(width, height)
-                        val displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize)
-                        val isCameraRotated = cameraHelper.isCameraRotated
-                        converter.setSurfaceTextureAndAttachToGLContext(
-                            previewFrameTexture,
-                            if (isCameraRotated) displaySize.height else displaySize.width,
-                            if (isCameraRotated) displaySize.width else displaySize.height
-                        )
-                        try {
-                            previewFrameTexture.updateTexImage()
-                        } catch (exception: Exception) {
-                            Log.d("HomeFragment", exception.message.orEmpty())
-                        }
-                    }
-
-                    override fun surfaceDestroyed(holder: SurfaceHolder) {
-                        Log.d("HomeFragment", "surface destroyed")
-                        processor.videoSurfaceOutput.setSurface(null)
-                    }
-                })
     }
 
     override fun onRequestPermissionsResult(
