51dfa82192c7311ece2fc2ed54536f38044ad025
Vladislav Gladkiy <v.a.gladkiy1@gmail.com>
2021-04-17 11:29:10 +0300

    Move MainActivity to ui

---
+++ app/src/main/java/com/android/signlanguage/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.android.signlanguage.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.signlanguage.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
