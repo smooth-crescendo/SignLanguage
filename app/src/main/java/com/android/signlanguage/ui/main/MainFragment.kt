3f0d9bdcb2bbab8d88fef465232c4e02aebabf42
Vladislav Gladkiy <v.a.gladkiy1@gmail.com>
2021-05-02 23:16:43 +0300

    Add button to reset progress

--- app/src/main/java/com/android/signlanguage/ui/main/MainFragment.kt
+++ app/src/main/java/com/android/signlanguage/ui/main/MainFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.android.signlanguage.R
 import com.android.signlanguage.databinding.FragmentMainBinding
+import com.android.signlanguage.model.skill.UserSkill
 
 class MainFragment : Fragment() {
 
@@ -32,13 +34,19 @@ class MainFragment : Fragment() {
     ): View? {
         val binding = FragmentMainBinding.inflate(inflater, container, false)
 
-        _viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val factory = MainViewModelFactory(UserSkill.getInstance(requireContext()))
+        _viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
 
         binding.lifecycleOwner = this
         binding.viewModel = _viewModel
 
         _viewModel.lessonStarted = {
             findNavController().navigate(R.id.action_mainFragment_to_lessonFragment)
+        }
+
+        _viewModel.progressReset = {
+            UserSkill.save(requireContext())
+            Toast.makeText(context, "Progress has been reset", Toast.LENGTH_SHORT).show()
         }
 
         return binding.root
--- app/src/main/java/com/android/signlanguage/ui/main/MainViewModel.kt
+++ app/src/main/java/com/android/signlanguage/ui/main/MainViewModel.kt
@@ -1,11 +1,25 @@
 package com.android.signlanguage.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.android.signlanguage.model.skill.UserSkill
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val userSkill: UserSkill) : ViewModel() {
     var lessonStarted: (() -> Unit)? = null
+    var progressReset: (() -> Unit)? = null
 
     fun startLesson() {
         lessonStarted?.invoke()
+    }
+
+    fun resetProgress() {
+        userSkill.languages.clear()
+        progressReset?.invoke()
+    }
+}
+
+class MainViewModelFactory(val userSkill: UserSkill) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(userSkill) as T
     }
 }
